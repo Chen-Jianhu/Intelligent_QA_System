3b97705e7b77c7ccc6d1ebacda22a70a5cd25f8d
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-06-17 17:45:37 +0800

    Update models.py
    重新设计用户模型

--- user/models.py
+++ user/models.py
@@ -6,11 +6,24 @@ class UserInfoManager(models.Manager):
         user = UserInfo()
         user.name = account['name']
         user.pwd = account['pwd']
+        if account['sex']:
+            user.sex = account['sex']
+        if account['age']:
+            user.age = account['age']
+        if account['email']:
+            user.email = account['email']
+        if account['img_path']:
+            user.sex = account['img_path']
         return user
 
+
 class UserInfo(models.Model):
-    name = models.CharField(max_length=20)
-    pwd = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
+    pwd = models.CharField(max_length=30)
+    sex = models.BooleanField(default=True)  # True 男
+    age = models.IntegerField(default=18)
+    email = models.CharField(max_length=30, null=True, blank=True)
+    img_path = models.CharField(max_length=100, default='/static/user/img/user.png')
     manager = UserInfoManager()
 
     def __str__(self):
